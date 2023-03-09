beb36288b4ede94b3f6ad64a04888dcf84e44124
jeyjy <jeyjy0810@gmail.com>
2023-03-09 20:31:42 +0800

    更新依赖关系

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -3,13 +3,19 @@ import { RouteRecordRaw } from 'vue-router';
 
 export const routes: Array<RouteRecordRaw> = [
     // 重定向
-    {
-        path    : '',
-        redirect: '/login'
-    },
+    // {
+    //     path    : '/',
+    //     redirect: '/login'
+    // },
+    // 登录
     // 登录
     {
-    // 主页
+        path     : '/login',
+        name     : 'login',
+        component: () => import('../views/Login/index.vue')
+    },
+    {
+        // 主页
         path     : '/home',
         name     : 'home',
         component: () => import('../components/Home/index.vue'),
